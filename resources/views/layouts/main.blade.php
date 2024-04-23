43c84b3054422e8225abb6466e8d458d7ea3e348
delmonicoCL <taiker17@gmail.com>
2024-04-23 13:30:24 +0200

    Cambios MAIN

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,13 @@
             <title>
                 @yield('titulo')
             </title>
-            @vite( [ "resources/css/app.css", "resources/css/app.scss", "resources/js/app.js",],)
+             @vite( [ "resources/css/app.css", "resources/css/app.scss", "resources/js/app.js",],) 
+
+            {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> --}}
+
+
+
            <link rel="stylesheet" href="{{ asset('css/autoevaluacion.css') }}" />
     </head>
     <body>
@@ -32,7 +38,7 @@
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
                                     data-bs-toggle="dropdown" aria-expanded="false">
-                                    Datos maestros
+                                    Datos XXX
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
                                     <li><a class="dropdown-item" href="#">Tipo usuarios</a></li>
