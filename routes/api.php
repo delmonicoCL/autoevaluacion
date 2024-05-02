eaf7aba54c39609396a8cb1d51f29176ee91bb09
Moheto22 <mohatae61@gmail.com>
2024-05-02 20:19:36 +0200

    API resoursesssss

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\CicleController;
+use App\Http\Controllers\Api\CriteriosAutoEvaluacionController;
+use App\Http\Controllers\Api\ResultadosAprendizajeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserHasModulsController;
@@ -12,3 +14,5 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('usuaris_has_moduls', UserHasModulsController::class);
 Route::apiResource('cicle', CicleController::class);
+Route::apiResource('resultats_aprenentatge', ResultadosAprendizajeController::class);
+Route::apiResource('criteris_avaluacio', CriteriosAutoEvaluacionController::class);
